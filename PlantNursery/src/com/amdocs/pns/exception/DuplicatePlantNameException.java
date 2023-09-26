8cb1c93df1ee925ab0408e0e1081ed3191a6fc49
Nitin Jeremiah <62741538+NitinJV18@users.noreply.github.com>
2023-09-26 12:27:57 +0530

    Delete PlantNursery directory

--- PlantNursery/src/com/amdocs/pns/exception/DuplicatePlantNameException.java
+++
@@ -1,7 +0,0 @@
-package com.amdocs.pns.exception;
-
-public class DuplicatePlantNameException extends Exception {
-    public DuplicatePlantNameException(String message) {
-        super(message);
-    }
-}
--- PlantNursery/src/com/amdocs/pns/exception/PlantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.amdocs.pns.exception;
-
-public class PlantNotFoundException extends Exception {
-    public PlantNotFoundException(String message) {
-        super(message);
-    }
-}
